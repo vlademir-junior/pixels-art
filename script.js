a00f6417f7f73c34d754944c805062804877dfc3
vlademir-junior <vlademir.junior.nh@gmail.com.br>
2023-05-17 15:47:19 -0300

    resolucao requisito 3

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function createPalettes() {
   const colorPalette = document.createElement("div");
   colorPalette.id = "color-palette";
-
-  const colorOptions = ["red", "blue", "green", "yellow"]; // Cores disponíveis na paleta
-
-  colorOptions.forEach((color) => {
+  const colorOptions = ["black", "red", "blue", "green"]; // Cores disponíveis na paleta
+  colorOptions.forEach((color, index) => {
     if (color !== "white") {
       const colorDiv = document.createElement("div");
       colorDiv.classList.add("color");
       colorDiv.style.backgroundColor = color;
       colorDiv.style.border = "1px solid black";
 
+      if (index === 0) {
+        colorDiv.style.backgroundColor = "black"; // Define o background preto para a primeira cor
+      }
       colorPalette.appendChild(colorDiv);
     }
   });
-
   const title = document.querySelector("h1");
   title.insertAdjacentElement("afterend", colorPalette);
 }
